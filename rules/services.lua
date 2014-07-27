bac78bd17735130ff0d8bc6934adfd20153ce1b1
Ard van Breemen <ard@kwaak.net>
2014-07-27 10:24:38 +0200

    Example rules and more servers
    First needed implementation
    More default services

--- rules/services.lua
+++ rules/services.lua
@@ -4,3 +4,5 @@ s:new{"ssh",proto="tcp",port=22}
 s:new{"http",proto="tcp",port=80}
 s:new{"https",proto="tcp",port=443}
 s:new{"sip",proto={"tcp","udp"},port=5060}
+s:new{"iax2",proto="udp",port=4569}
+s:new{"dundi",proto="udp",port=4520}
