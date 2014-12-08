34f0b92614adec8ec6094de550d46d3bc12834fa
Ard van Breemen <ard@kwaak.net>
2014-12-08 13:19:48 +0100

    zerobrane debug starter

---
+++ zbrun.lua
@@ -0,0 +1,5 @@
+package.path=package.path ..";/usr/share/lua/5.2/?.lua;/usr/share/lua/5.2/?/init.lua;./?.lua"
+package.cpath=package.cpath ..";/usr/lib/arm-linux-gnueabihf/lua/5.2/?.so"
+rulesdir="rules"
+scriptsdir="scripts"
+dofile("firewall.lua")
