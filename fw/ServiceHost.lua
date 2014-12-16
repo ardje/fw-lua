8b1863b6c76c5078f3c13bfdfb27aecb1561dfeb
Ard van Breemen <ard@kwaak.net>
2014-12-16 17:10:01 +0100

    Wrong sugarsyntax

--- fw/ServiceHost.lua
+++ fw/ServiceHost.lua
@@ -1,6 +1,6 @@
 local host=require"fw.Host"
 local M=host:New{"ServiceHost"}
-function M:Service{r}
+function M:Service(r)
 	local source=r[1]
 	for n=2,#r do
 		local aservice=r[n]
@@ -8,7 +8,7 @@ function M:Service{r}
 		self:dnatTo{source,aservice}
 	end
 end
-function M:DNatMe{r}
+function M:DNatMe(r)
 	local source=r[1]
 	for n=2,#r do
 		local aservice=r[n]
