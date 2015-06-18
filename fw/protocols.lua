85aada858457a05b4cc91c83c0e4ea0be4c5a896
Ard van Breemen <ard@kwaak.net>
2015-06-18 11:50:58 +0200

    Add syslog protocol

--- fw/protocols.lua
+++ fw/protocols.lua
@@ -14,4 +14,5 @@ return {
 	ntp=s:new{"ntp",proto={"udp"},port=123},
 	rdp=s:new{"rdp",proto={"tcp"},port=3389},
 	svn=s:new{"svn",proto={"tcp"},port=3690}
+	syslog=s:new{"svn",proto={"udp"},port=514}
 }
