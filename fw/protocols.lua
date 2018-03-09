228859ac7ec2189245eac16dfd63b60837271b45
Ard van Breemen <ard@kwaak.net>
2018-03-09 14:24:36 +0100

    Add httpalt

--- fw/protocols.lua
+++ fw/protocols.lua
@@ -3,6 +3,7 @@ return {
 	ssh=s:new{"ssh",proto="tcp",port=22},
 	http=s:new{"http",proto="tcp",port=80},
 	https=s:new{"https",proto="tcp",port=443},
+	httpalt=s:new{"httpalt",proto="tcp",port=8080},
 	sip=s:new{"sip",proto={"tcp","udp"},port=5060},
 	iax2=s:new{"iax2",proto="udp",port=4569},
 	dundi=s:new{"dundi",proto="udp",port=4520},
