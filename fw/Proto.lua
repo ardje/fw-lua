d28e5cd3b4b922941baa681a00dae3c2c5aeae36
Ard van Breemen <ard@kwaak.net>
2018-07-16 12:02:41 +0200

    portless protocols

--- fw/Proto.lua
+++ fw/Proto.lua
@@ -1,10 +1,18 @@
 local Object=require"fw.Object"
 local M=Object:New("Proto")
 function M:asSource()
-	return { f=1,"--proto",self.proto,"--source-port",self.port }
+	if self.port then
+		return { f=1,"--proto",self.proto,"--source-port",self.port }
+	else
+		return { f=1,"--proto",self.proto }
+	end
 end
 function M:asDestination()
-	return { f=1,"--proto",self.proto,"--destination-port",self.port }
+	if self.port then
+		return { f=1,"--proto",self.proto,"--destination-port",self.port }
+	else
+		return { f=1,"--proto",self.proto }
+	end
 end
 M.asDefault=M.asDestination
 return M
