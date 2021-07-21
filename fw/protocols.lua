0fc81be39ac7f5766d8c0e480f8810ec407d5836
Ard van Breemen @ lenny <ard@kwaak.net>
2021-07-21 15:47:39 +0200

    pay attention

--- fw/protocols.lua
+++ fw/protocols.lua
@@ -34,5 +34,5 @@ return {
 	openvpnudp=s:new{"openvpnudp",proto={"udp"},port=1194},
 	wireguard=s:new{"wireguard",proto={"udp"},port=51820},
 	mssql=s:new{"mssql",proto={"tcp"},port=1433},
-	mqtt=s:new{"mssql",proto={"tcp"},port=1883},
+	mqtt=s:new{"mqtt",proto={"tcp"},port=1883},
 }
