bbe1e9d2b751d62b99445cf0dcee22c724d09473
Ard van Breemen @ lenny <ard@kwaak.net>
2022-06-17 18:35:24 +0200

    smtps

--- fw/protocols.lua
+++ fw/protocols.lua
@@ -8,6 +8,7 @@ return {
 	iax2=s:new{"iax2",proto="udp",port=4569},
 	dundi=s:new{"dundi",proto="udp",port=4520},
 	smtp=s:new{"smtp",proto="tcp",port=25},
+	smtps=s:new{"smtps",proto="tcp",port=465},
 	dns=s:new{"dns",proto={"tcp","udp"},port=53},
 	bootps=s:new{"bootps",proto={"udp"},port=67},
 	bootpc=s:new{"bootpc",proto={"udp"},port=68},
